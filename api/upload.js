591b9b397fde5d5f498c0b29aa4fcdf75ae49b5b
pqxzzz <pericles.code@gmail.com>
2025-09-15 15:35:15 +0100

    deploy vercel

--- api/upload.js
+++ api/upload.js
@@ -2,6 +2,7 @@
 // instalar: npm i formidable
 // api/upload.js
 import formidablePkg from "formidable";
+import fs from "fs/promises";
 const { IncomingForm } = formidablePkg;
 import { parseExcelBuffer } from "../src/parser.js";
 import { exportToExcelBuffer } from "../src/exporter.js";
@@ -14,14 +15,17 @@ export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(405).send("Método não permitido");
 
-  const form = new IncomingForm({ multiples: false });
+  const form = new IncomingForm({
+    multiples: false,
+    keepExtensions: true // mantém extensão do arquivo
+  });
 
   form.parse(req, async (err, fields, files) => {
     if (err) return res.status(500).send("Erro no parse do formulário");
 
     try {
-      const file = files.file; // name="file"
-      const buffer = await file.toBuffer(); // <-- funciona na v2
+      const file = files.file; // name="file" do input
+      const buffer = await fs.readFile(file.filepath); // <-- ler arquivo temporário
 
       const data = await parseExcelBuffer(buffer);
       const outBuffer = await exportToExcelBuffer(data);
